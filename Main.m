8e1786dfa6fa2a747ff4153e9a6e628e2c3b016d
RENCHUNZI-XIE <renchunzi.xie@gmail.com>
2020-11-05 22:43:53 +0800

    refine

--- Main.m
+++ Main.m
@@ -3,15 +3,15 @@ clear
 addpath modelPackages
 dm = DataManipulator('./data');
 %%--------dataset---------%%
-% dm.loadWeatherDataset();
+dm.loadWeatherDataset();
 % dm.loadSeaDataset();
 % dm.loadHyperplaneDataset();
-dm.loadKDDDataset();
+% dm.loadKDDDataset();
 % dm.loadSUSYDataset();
 % dm.loadKittiDataset();
 % dm.loadHepmassDataset();
 %%--------dataset---------%%
 dm.normalize();
 epochs = 1;
-nRun = 3;
+nRun = 5;
 AOMSDA
